ea5e4b41a0b3326946808154a61bf79620d8361b
N3mxara <nimsara.20223294@iit.ac.lk>
2023-05-22 14:06:02 +0530

    Update Mapping.jsx

--- src/Mapping.jsx
+++ src/Mapping.jsx
@@ -1,15 +1,17 @@
 function Mapping() {
-    const list=[1,2,3,4,5,6]
-    // const list2=list.map((number)=>number**2)
-    // console.log(list2);
-    const listItems = list.map((number)=><li>{number}</li>)
-    return(
-        <div>
-            <ul>
-                {listItems}
-            </ul>
-        </div>
-    )
+  const list = [1, 2, 3, 4, 5, 6];
+  // const list2=list.map((number)=>number**2)
+  // console.log(list2);
+  const listItems = list.map((number) => (
+    <li key={number}>
+      {number}*2={number * 2}
+    </li>
+  ));
+  return (
+    <div>
+      <ul>{listItems}</ul>
+    </div>
+  );
 }
 
 export default Mapping;
