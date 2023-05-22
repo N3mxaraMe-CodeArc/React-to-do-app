46dce559561807bf66156ffa30e1cab87a7fed89
N3mxara <nimsara.20223294@iit.ac.lk>
2023-05-22 21:20:42 +0530

    git push -u origin main

--- src/Hook.jsx
+++ src/Hook.jsx
@@ -2,17 +2,25 @@ import { useState, useEffect } from "react";
 
 function Hooks() {
   const [BodyText, setBodyText] = useState("Users");
+  const [first, setfirst] = useState(0);
 
   useEffect(() => {
     console.log("Use effect is Working");
-  })
- 
+  });
+
   return (
-    <div>
+    <div
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "200px",
+      }}
+    >
       <div>
         <button
           onClick={() => {
             setBodyText("Users");
+            setfirst(first + 1);
           }}
         >
           User
@@ -20,6 +28,7 @@ function Hooks() {
         <button
           onClick={() => {
             setBodyText("Posts");
+            setfirst(first + 1);
           }}
         >
           Post
@@ -27,14 +36,31 @@ function Hooks() {
         <button
           onClick={() => {
             setBodyText("AboutUS");
+            setfirst(first + 1);
           }}
         >
           About us
         </button>
       </div>
       <h1>{BodyText}</h1>
+      <button
+        onClick={() => {
+          setfirst(first + 1);
+        }}
+      >
+        Click Me
+      </button>
+      <button
+        onClick={() => {
+          setfirst(first - 1);
+        }}
+      >
+        Remove Count
+      </button>
+      <h2>Count : {first}</h2>
     </div>
   );
 }
+
 
 export default Hooks;
