46766819a867fa1c6cf8fd5d0b2fef89049b18fc
N3mxara <nimsara.20223294@iit.ac.lk>
2023-05-23 15:48:34 +0530

    git push -u origin main

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import React from 'react';
 // import Cricketdet from './Cricketde';
 // import pathum from './assets/pathum.png';
 // import sanga from './assets/sanga.png';
-import mali from './assets/mali.png';
+// import mali from './assets/mali.png';
 // import CricketDemo from './cricketpct';
 // import Hooks from './Hook';
-import Card from './Card';
+// import Card from './Card';
+import { Routes } from 'react-router-dom';
 
 function App() {
   
@@ -21,37 +22,39 @@ function App() {
   //   return (num**2);
   // })
 
+  // 
   
-  
 
-  return (
-    <div>
+  return(
+    <Routes>
       
-
-
-
-
-
-
-
-
-
-
-
-      <Card image={mali} name="Lasith Malinga" Age="38"/>
-      {/* <ToDoItem taskName='Task01' btnName='Done' btnColor='red' />
-      <ToDoItem taskName='Task02' btnName='ToDo' btnColor='green' /> */}
-      {/* <Cricketdet Name="Pathum Nissanka" innScore="29" score="768" avePercent="29.8" image={pathum} altText="Pathum Nissanka" /> */}
-      {/* <Cricketdet Name="Lasith malinga" innScore="39" score="968" avePercent="49.8" image={mali} altText="Lasith Malinga" /> */}
-      {/* <Cricketdet Name="Kumar Sangakkara" innScore="59" score="1768" avePercent="32.8" image={sanga} altText="Kumar Sangakkara" /> */}
-      {/* <CricketDemo photo={pathum} name="Pathum Nissanka" Score="395" inningScore="27.8" />
-      <CricketDemo photo={sanga} name="Kumar Sangakkara" Score="3700" inningScore="97.8" />
-      <CricketDemo photo={mali} name="LAsith Malinga" Score="395" inningScore="27.8" /> */}
-      {/* <Hooks /> */}
-      
-    </div>
+    </Routes>
   );
-}
+};
+
+
+
+
+
+
+
+
+
+
+      // // <Card image={mali} name="Lasith Malinga" Age="38"/>
+      // {/* <ToDoItem taskName='Task01' btnName='Done' btnColor='red' />
+      // <ToDoItem taskName='Task02' btnName='ToDo' btnColor='green' /> */}
+      // {/* <Cricketdet Name="Pathum Nissanka" innScore="29" score="768" avePercent="29.8" image={pathum} altText="Pathum Nissanka" /> */}
+      // {/* <Cricketdet Name="Lasith malinga" innScore="39" score="968" avePercent="49.8" image={mali} altText="Lasith Malinga" /> */}
+      // {/* <Cricketdet Name="Kumar Sangakkara" innScore="59" score="1768" avePercent="32.8" image={sanga} altText="Kumar Sangakkara" /> */}
+      // {/* <CricketDemo photo={pathum} name="Pathum Nissanka" Score="395" inningScore="27.8" />
+      // <CricketDemo photo={sanga} name="Kumar Sangakkara" Score="3700" inningScore="97.8" />
+      // <CricketDemo photo={mali} name="LAsith Malinga" Score="395" inningScore="27.8" /> */}
+      // {/* <Hooks /> */}
+      
+    // </div>
+  // );
+// }
 
 
 export default App;
