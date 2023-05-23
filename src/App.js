ae8a018e50403772877b0b9bc2c9117e84672d8e
N3mxara <nimsara.20223294@iit.ac.lk>
2023-05-23 12:44:44 +0530

    git push -u origin main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import ToDo from './containers/ToDo.js';
-import './App.css';
+// import './App.css';
 // import Mapping from './Mapping'
 // import ToDoItem from './ToDoItem.jsx';
 // import Cricketdet from './Cricketde';
@@ -24,21 +24,19 @@ function App() {
 
   return (
     <div>
+
+
+
+
+
+
+
+
+
+
+
+
       {/* <CardSet /> */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
       {/* <ToDoItem taskName='Task01' btnName='Done' btnColor='red' />
       <ToDoItem taskName='Task02' btnName='ToDo' btnColor='green' /> */}
       {/* <Cricketdet Name="Pathum Nissanka" innScore="29" score="768" avePercent="29.8" image={pathum} altText="Pathum Nissanka" /> */}
@@ -47,7 +45,6 @@ function App() {
       {/* <CricketDemo photo={pathum} name="Pathum Nissanka" Score="395" inningScore="27.8" />
       <CricketDemo photo={sanga} name="Kumar Sangakkara" Score="3700" inningScore="97.8" />
       <CricketDemo photo={mali} name="LAsith Malinga" Score="395" inningScore="27.8" /> */}
-
       {/* <Hooks /> */}
       
     </div>
