eb766a7b4930cad92fc88eb501b352af58e2bf65
N3mxara <nimsara.20223294@iit.ac.lk>
2023-05-24 19:30:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,13 +18,24 @@ import { useState } from 'react';
 function App() {
 
 
-  const [Data, setData] = useState();
+  const [data, setdata] = useState();
 
-  setData(
-    axios.get("https://api.restfull-api.dev/objects/7")
-    
-  )
-  
+
+
+
+  const Data = async()=>{
+    axios.get("https://api.restful-api.dev/objects").then((response) => {
+    setdata(response.data[0].name);
+    console.log(response.data[0].name);
+  }).catch((error)=>{
+    console.log(error);
+  });
+  }
+
+
+
+
+
   // const numArray = [1,2,3,4,5];
   // const CardSet = numArray.map((num)=>{
   //   return <Card number={num} />
@@ -34,13 +45,13 @@ function App() {
   // })
 
   // 
-  
 
-  return(
+
+  return (
     <div className='background'>
-      <div className='form-title'>
-        
-      </div>
+      <div className='deviceName'>Device Name</div>
+      <div className='ddevice-color'>Device Color</div>
+      <div className='device-capacity'>Device-Capacity</div>
     </div>
   );
 };
@@ -54,19 +65,19 @@ function App() {
 
 
 
-      // // <Card image={mali} name="Lasith Malinga" Age="38"/>
-      // {/* <ToDoItem taskName='Task01' btnName='Done' btnColor='red' />
-      // <ToDoItem taskName='Task02' btnName='ToDo' btnColor='green' /> */}
-      // {/* <Cricketdet Name="Pathum Nissanka" innScore="29" score="768" avePercent="29.8" image={pathum} altText="Pathum Nissanka" /> */}
-      // {/* <Cricketdet Name="Lasith malinga" innScore="39" score="968" avePercent="49.8" image={mali} altText="Lasith Malinga" /> */}
-      // {/* <Cricketdet Name="Kumar Sangakkara" innScore="59" score="1768" avePercent="32.8" image={sanga} altText="Kumar Sangakkara" /> */}
-      // {/* <CricketDemo photo={pathum} name="Pathum Nissanka" Score="395" inningScore="27.8" />
-      // <CricketDemo photo={sanga} name="Kumar Sangakkara" Score="3700" inningScore="97.8" />
-      // <CricketDemo photo={mali} name="LAsith Malinga" Score="395" inningScore="27.8" /> */}
-      // {/* <Hooks /> */}
-      
-    // </div>
-  // );
+// // <Card image={mali} name="Lasith Malinga" Age="38"/>
+// {/* <ToDoItem taskName='Task01' btnName='Done' btnColor='red' />
+// <ToDoItem taskName='Task02' btnName='ToDo' btnColor='green' /> */}
+// {/* <Cricketdet Name="Pathum Nissanka" innScore="29" score="768" avePercent="29.8" image={pathum} altText="Pathum Nissanka" /> */}
+// {/* <Cricketdet Name="Lasith malinga" innScore="39" score="968" avePercent="49.8" image={mali} altText="Lasith Malinga" /> */}
+// {/* <Cricketdet Name="Kumar Sangakkara" innScore="59" score="1768" avePercent="32.8" image={sanga} altText="Kumar Sangakkara" /> */}
+// {/* <CricketDemo photo={pathum} name="Pathum Nissanka" Score="395" inningScore="27.8" />
+// <CricketDemo photo={sanga} name="Kumar Sangakkara" Score="3700" inningScore="97.8" />
+// <CricketDemo photo={mali} name="LAsith Malinga" Score="395" inningScore="27.8" /> */}
+// {/* <Hooks /> */}
+
+// </div>
+// );
 // }
 
 
